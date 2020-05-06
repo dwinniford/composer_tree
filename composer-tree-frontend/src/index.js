ff1bd4552b3c092008fc016d38885ad8cb24a991
Dave Winniford <dwinniford@gmail.com>
2020-05-06 09:34:44 -0500

    network has click listener that logs note id

--- composer-tree-frontend/src/index.js
+++ composer-tree-frontend/src/index.js
@@ -209,6 +209,7 @@ class Network {
     }
     
     createAdaptedNoteArray() { 
+        // refactor with map
         const nodeArray = [] 
         this.noteArray.forEach(function(note) {
             nodeArray.push({id: note.id, label: note.title})
@@ -249,35 +250,10 @@ class Network {
 
         // initialize your network!
         const network = new vis.Network(container, data, options);
+        network.on("click", function(event) {
+
+            console.log(event.nodes[0])
+            // logs note id.  If click on canvas but not a circle logs undefined. if click on line stil undefined.
+        })
     }
 }
-
-// // create an array with nodes
-// const nodes = new vis.DataSet([
-//     {id: 1, label: 'Node 1'},
-//     {id: 2, label: 'Node 2'},
-//     {id: 3, label: 'Node 3'},
-//     {id: 4, label: 'Node 4'},
-//     {id: 5, label: 'Node 5'}
-// ]);
-
-// // create an array with edges
-// const edges = new vis.DataSet([
-//     {from: 1, to: 3},
-//     {from: 1, to: 2},
-//     {from: 2, to: 4},
-//     {from: 2, to: 5}
-// ]);
-
-// // create a network
-// const container = document.getElementById('mynetwork');
-
-// // provide the data in the vis format
-// const data = {
-//     nodes: nodes,
-//     edges: edges
-// };
-// const options = {};
-
-// // initialize your network!
-// const network = new vis.Network(container, data, options);
