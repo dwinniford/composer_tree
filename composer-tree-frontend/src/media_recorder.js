453eca051ef3c0565581d18b46693fa3bc58bd80
Dave Winniford <dwinniford@gmail.com>
2020-05-11 10:42:37 -0500

    record buttons change disable boolean

--- composer-tree-frontend/src/media_recorder.js
+++ composer-tree-frontend/src/media_recorder.js
@@ -1,2 +1,15 @@
 const recordButton = document.querySelector("#record")
 const stopRecordButton = document.querySelector("#stopRecord")
+
+recordButton.addEventListener("click", function(event) {
+    console.log(event.currentTarget)
+    recordButton.disabled = true;
+    stopRecordButton.disabled = false;
+})
+
+stopRecordButton.addEventListener("click", function(event) {
+    console.log(event.currentTarget);
+    stopRecordButton.disabled = true;
+    recordButton.disabled = false;
+})
+
