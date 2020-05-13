722a44ec74e9b9b73c5c2712fd169487a5e4661a
Dave Winniford <dwinniford@gmail.com>
2020-05-13 11:06:05 -0500

    signup form has submit listener

--- composer-tree-frontend/src/user.js
+++ composer-tree-frontend/src/user.js
@@ -10,6 +10,10 @@ class User {
         userForm.innerHTML = ''
         userForm.appendChild(formElement)
         // add form listener
+        formElement.addEventListener("submit", function(event) {
+            event.preventDefault();
+            console.log(event.currentTarget.action)
+        })
     }
 
     static fieldsArray() {
