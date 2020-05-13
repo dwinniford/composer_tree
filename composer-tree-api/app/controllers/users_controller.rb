27dc3b5fdbe94d2d5c04b837184289a55826e393
Dave Winniford <dwinniford@gmail.com>
2020-05-13 13:51:20 -0500

    signup form creates user with secure pw

--- composer-tree-api/app/controllers/users_controller.rb
+++ composer-tree-api/app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :email, :image, :uid, :password_digest)
+      params.require(:user).permit(:name, :email, :image, :uid, :password)
     end
 end
