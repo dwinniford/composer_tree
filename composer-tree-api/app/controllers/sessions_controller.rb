c6b601099d3b46e3c14948f19493bcf8f34b8660
Dave Winniford <dwinniford@gmail.com>
2020-05-14 09:56:44 -0500

    adds sessions create backend

--- composer-tree-api/app/controllers/sessions_controller.rb
+++ composer-tree-api/app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find_by(name: params[:user][:name], email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            render json: @user
+        else 
+            render json: @user.errors
+        end
   end
 
   def destroy
